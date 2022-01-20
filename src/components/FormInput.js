0b3cbd5e5b8b50f812c411ff8261621d5eb846c2
Andrew Corbin <andrew.c.corbin@gmail.com>
2022-01-20 11:22:18 -0600

    Added component.

---
+++ src/components/FormInput.js
@@ -0,0 +1,20 @@
+
+const formInput = (props) => {
+    return (
+      <div>
+        <div className="field">
+         <label> Customer Name:</label>
+         <input
+          type="text"
+          name={props.name}
+          placeholder={props.placeholder}
+          value= {props.formValues.username}
+          onChange={props.handleChange}
+         />
+         </div>
+         <p>{props.formErrors.username}</p>
+      </div>
+    );
+}
+
+export default formInput
